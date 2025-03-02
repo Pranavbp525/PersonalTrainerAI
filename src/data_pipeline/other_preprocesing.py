19c9665c844e8e35897c24cb763bd1953dd85220
RuthvikaReddyTangirala <ruthvikareddytangirala20@gmail.com>
2025-03-02 13:54:50 -0500

    updated other preprocessing

--- src/data_pipeline/other_preprocesing.py
+++ src/data_pipeline/other_preprocesing.py
@@ -7,9 +7,9 @@ import logging
 # Constants and Directories
 # ----------------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-INPUT_DIR = os.path.join(BASE_DIR, "data", "raw_json_data")
-OUTPUT_DIR = os.path.join(BASE_DIR, "data", "preprocessed_json_data")
-LOG_FILE = os.path.join(BASE_DIR, "preprocessing.log")
+INPUT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/raw_json_data"))
+OUTPUT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/preprocessed_json_data"))
+LOG_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../logs/preprocessing.log"))
 
 # ----------------------------------
 # Logging Configuration
